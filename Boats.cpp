37cef69c5fd16dbe64d29a3277ec45cd5fd08996
SaranshGupta6 <83112564+SaranshGupta6@users.noreply.github.com>
2022-03-25 21:26:44 +0530

    Boats to Save People

---
+++ Boats.cpp
@@ -0,0 +1,18 @@
+ // Time Complexity: O(NlogN), where N is the length of people.
+
+//Space Complexity: O(N)
+    
+        int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1;
+        int ans = 0;
+
+        while (i <= j) {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return ans;
+        }
